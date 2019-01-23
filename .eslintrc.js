fd071ae004abf455be0f906017b7cfffefcfdd9d
Richard Murnane <richard.murnane@internode.on.net>
2019-01-23 20:16:56 +1100

    ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+
+  parser: "babel-eslint",
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
+  env: {
+    node: true
+  },
+
+  rules: {
+    quotes: [
+      "error",
+      "double",
+      {
+        avoidEscape: true
+      }
+    ],
+
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ]
+  },
+
+  settings: {
+    react: {
+      version: "16.7.0"
+    }
+  }
+};
