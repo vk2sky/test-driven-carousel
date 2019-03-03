81de3fec73da28f25b1b0922a9af429d4ee62709
Richard Murnane <richard.murnane@internode.on.net>
2019-03-04 08:53:36 +1100

    :memo: Build storybook docs

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,9 +1,18 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, number } from "@storybook/addon-knobs";
 import Carousel from "../src/Carousel";
 import slides from "../example/slides";
 
-storiesOf("Carousel", module)
-  .add("default", () => <Carousel slides={slides} />
+const stories = storiesOf("Carousel", module);
+
+stories.addDecorator(withKnobs);
+
+stories.add("default", () =>
+  <Carousel
+    autoAdvanceDelay={number("autoAdvanceDelay", 10e3)}
+    slides={slides}
+    onIndexChange={action("onIndexChange")}
+  />
 );
